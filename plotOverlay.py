4b91605b1ed232462d130885512909abdc718501
Christopher_Madrid <chrismadrid173@gmail.com>
2025-03-26 11:03:09 -0500

    Trying to clean up the code and added overlay code

---
+++ plotOverlay.py
@@ -0,0 +1,71 @@
+import ROOT
+import re
+
+# File and histogram base name
+file_path = "outfile.root"
+histogram_suffix = "nPhotonsPerChannel"
+
+# Open ROOT file
+f = ROOT.TFile.Open(file_path)
+if not f or f.IsZombie():
+    print("Error opening file!")
+    exit()
+
+# Get list of keys and filter histograms
+hist_names = [key.GetName() for key in f.GetListOfKeys()]
+matching_hists = [name for name in hist_names if name.startswith(histogram_suffix)]
+
+def extract_sort_key(name):
+    match = re.match(histogram_suffix + r"(\d+)x(\d+)_", name)
+    if match:
+        return int(match.group(1))  # Change to group(2) if sorting by second y
+    return float('inf')  # Push non-matching names to the end
+
+# Sort histograms by the extracted number
+matching_hists.sort(key=extract_sort_key)
+
+# Prepare canvas and legend
+canvas = ROOT.TCanvas("canvas", "Overlayed Histograms", 800, 600)
+canvas.SetLogy()
+canvas.SetTickx()
+canvas.SetTicky()
+legend = ROOT.TLegend(0.65, 0.5, 0.88, 0.88)
+legend.SetBorderSize(0)
+legend.SetFillStyle(0)
+
+# Colors for plotting
+colors = [ROOT.kRed, ROOT.kBlue, ROOT.kGreen+2, ROOT.kMagenta, ROOT.kOrange+7, ROOT.kCyan+2]
+
+# Draw histograms
+for i, hist_name in enumerate(matching_hists):
+    hist = f.Get(hist_name)
+    if not hist:
+        continue
+
+    if hist.Integral() > 0:
+        hist.Scale(1.0/hist.Integral())
+
+    hist.SetLineColor(colors[i % len(colors)])
+    hist.SetLineWidth(2)
+    hist.SetStats(0)
+
+    # Set axis titles only once
+    if i == 0:
+        hist.GetXaxis().SetTitle("NPhotons per channel")
+        hist.GetYaxis().SetTitle("Normalized")
+        hist.Draw("HIST")
+    else:
+        hist.Draw("HIST SAME")
+
+    # Add legend entry with prefix only
+    prefix = hist_name.replace(histogram_suffix + "_", "") + (" #mum^{2}")
+    legend.AddEntry(hist, prefix, "l")
+
+
+f.Get(matching_hists[0]).Draw("AXIS SAME")
+legend.Draw()
+#canvas.SetGrid()
+canvas.Update()
+
+# Save output if needed
+canvas.SaveAs("overlayed_histograms.png")
--- simDigitalSiPM.py
+++ simDigitalSiPM.py
@@ -7,18 +7,17 @@ import os
 ROOT.gROOT.SetBatch(True)
 ROOT.TH1.SetDefaultSumw2()
 
-def makeBranch(tree, name, t):
-    b = None
-    if t == "vector":
-        b = ROOT.vector('double')()
-        tree.Branch(name, b)
-    elif t == "D":
-        b = np.zeros(1, dtype=float)
-        tree.Branch(name, b, name+"/"+t)
-    else:
-        print("Branch type not supported yet. Add it here")
-    return b
-    
+class Photons:
+    def __init__(self, event):
+        self.isCoreC = np.array(event.OP_isCoreC)
+        self.pos_final_x = np.array(event.OP_pos_final_x)
+        self.pos_final_y = np.array(event.OP_pos_final_y)
+        self.pos_final_z = np.array(event.OP_pos_final_z)
+        self.time_final = np.array(event.OP_time_final)
+
+    def nPhotons(self):
+        return len(self.pos_final_x)
+
 def saveHisto(c, hDic, name, drawOpt="", doLog=False):
     hDic[name].Draw(drawOpt)
     hDic[name].SetStats(0)
@@ -47,11 +46,15 @@ def hitOneArray(h, x, y):
     return binNum==b
 
 def getSiPMResponse(time):
-    l = ROOT.TF1("landau","landau(0)",0, 25)
-    l.SetParameter(0, 1)
+    l = ROOT.TF1("landau","landau(0)+gaus(3)",0, 25)
+    l.SetParameter(0, 0.5)
     l.SetParameter(1, time)
-    l.SetParameter(2, 0.1)
+    l.SetParameter(2, 0.2)
+    l.SetParameter(3, 1)
+    l.SetParameter(4, time)
+    l.SetParameter(5, 0.25)
     l.SetNpx(500)
+
     h = ROOT.TH1D(l.GetHistogram())
     h.SetName("{}".format(time))
     if h.Integral()>0:
@@ -60,7 +63,8 @@ def getSiPMResponse(time):
 
 def main():
     # Some useful hardcoded stuff
-    input_file = ROOT.TFile("mc_testjob_run001_003_Test_20evt_pi+_100.0_100.0.root", "READ")
+    #input_file = ROOT.TFile("mc_testjob_run001_003_Test_20evt_pi+_100.0_100.0.root", "READ")
+    input_file = ROOT.TFile("mc_testjob_run001_003_Test_20evt_e+_100.0_100.0.root", "READ")
     tree = input_file.Get("tree")
     root_file = ROOT.TFile("outfile.root", "RECREATE")
     os.makedirs("output/", exist_ok=True)
@@ -69,82 +73,89 @@ def main():
     histos = {}
 
     nChannels = [
-        ("1000x1000", 1000), 
-        ("500x500", 500), 
-        ("250x250", 250), 
-        ("100x100", 100), 
-        ("75x75",    75), 
-        ("50x50",    50), 
-        ("25x25",    25), 
-        ("20x20",    20), 
-        ("15x15",    15), 
-        ("10x10",    10), 
-        ("5x5",       5), 
-        ("3x3",       3),
-        ("2x2",       2),
-        ("1x1",       1),
+        ("1x1",     3000), 
+        #("2x2",     1500), 
+        #("5x5",      600), 
+        ("10x10",    300), 
+        #("20x20",    150), 
+        ("25x25",    120), 
+        #("30x30",    100), 
+        #("40x40",     75), 
+        ("50x50",     60), 
+        #("60x60",     50), 
+        ("75x75",     40), 
+        ("100x100",   30), 
+        #("120x120",   25), 
+        #("125x125",   24), 
+        #("200x200",   15),
+        #("600x600",    5),
+        ("3000x3000",  1),
     ]
 
     for c in nChannels: histos["nPhotons_xy_{}".format(c[0])] = ROOT.TH2D("nPhotons_xy_{}".format(c[0]),"nPhotons_xy; x [cm]; y [cm]; nPhotons", c[1],-3.45,-3.15, c[1],3.5,3.8)
     for c in nChannels: histos["dummy_nPhotons_xy_{}".format(c[0])] = ROOT.TH2D("dummy_nPhotons_xy_{}".format(c[0]),"nPhotons_xy; x [cm]; y [cm]; nPhotons", c[1],-3.45,-3.15, c[1],3.5,3.8)
     for c in nChannels: histos["nPhotonsPerChannel_{}".format(c[0])] = ROOT.TH1D("nPhotonsPerChannel_{}".format(c[0]),"nPhotonsChannel; nPhotons", 20, 0, 20)
+    for c in nChannels: histos["nPhotonsPerChannel_time_{}".format(c[0])] = ROOT.TH2D("nPhotonsPerChannel_time_{}".format(c[0]),"nPhotonsChannel; nPhotons; time", 500,0.0,25.0, 20,0,20)
     for c in nChannels: histos["nPhotons_time_{}".format(c[0])] = ROOT.TH1D("nPhotons_time_{}".format(c[0]),"nPhotons_time; time [ns]; nPhotons", 500,0.0,25.0)
     for c in nChannels: histos["nPhotons_time_smear_{}".format(c[0])] = ROOT.TH1D("nPhotons_time_smear_{}".format(c[0]),"nPhotons_time_smear; time [ns]; nPhotons Smear", 500,0.0,25.0)
     for c in nChannels: histos["signal_time_{}".format(c[0])] = ROOT.TH1D("signal_time_{}".format(c[0]),"signal_time; time [ns]; Amplitude [mV]", 500,0.0,25.0)
 
     # Loop over events
+    nEvents = 0
     for event in tree:
-        OP_isCoreC = np.array(event.OP_isCoreC)
-        OP_pos_final_x = np.array(event.OP_pos_final_x)
-        OP_pos_final_y = np.array(event.OP_pos_final_y)
-        OP_pos_final_z = np.array(event.OP_pos_final_z)
-        OP_time_final = np.array(event.OP_time_final)
-        nPhotons = len(OP_pos_final_x)
-        print("Total number of photons:", nPhotons)
+        nEvents += 1
+        if nEvents < 7:
+            continue
+
+        if nEvents >= 8: break
+        # break
+
+        gammas = Photons(event)
+        print("Total number of photons:", gammas.nPhotons())
 
         #Loop over photons in the event
-        for i, _ in np.ndenumerate(OP_pos_final_x):
-            x = OP_pos_final_x[i]
-            y = OP_pos_final_y[i]
-            z = OP_pos_final_z[i]
-            t = OP_time_final[i]
-            isCoreC = bool(OP_isCoreC[i])
+        for i, _ in np.ndenumerate(gammas.pos_final_x):
+            x = gammas.pos_final_x[i]
+            y = gammas.pos_final_y[i]
+            z = gammas.pos_final_z[i]
+            t = gammas.time_final[i]
+            isCoreC = bool(gammas.isCoreC[i])
             for c in nChannels:
                 if z>0 and isCoreC:
                     histos["nPhotons_xy_{}".format(c[0])].Fill(x, y)
                     histos["dummy_nPhotons_xy_{}".format(c[0])].Fill(x, y)
 
-        for i, _ in np.ndenumerate(OP_pos_final_x):
-            x = OP_pos_final_x[i]
-            y = OP_pos_final_y[i]
-            z = OP_pos_final_z[i]
-            t = OP_time_final[i]
-            isCoreC = bool(OP_isCoreC[i])
+        for i, _ in np.ndenumerate(gammas.pos_final_x):
+            x = gammas.pos_final_x[i]
+            y = gammas.pos_final_y[i]
+            z = gammas.pos_final_z[i]
+            t = gammas.time_final[i]
+            isCoreC = bool(gammas.isCoreC[i])
             for c in nChannels:
                 if z>0 and isCoreC:
                     if(hitOneArray(histos["nPhotons_xy_{}".format(c[0])], x, y)): 
                         histos["nPhotons_time_{}".format(c[0])].Fill(t)
-                        rSiPM = getSiPMResponse(t)
-                        histos["nPhotons_time_smear_{}".format(c[0])].Add(rSiPM)
+        #                 rSiPM = getSiPMResponse(t)
+        #                 histos["nPhotons_time_smear_{}".format(c[0])].Add(rSiPM)
 
         for c in nChannels: 
-            #Make realistic waveform plots
-            histos["signal_time_{}".format(c[0])] = noise1D(histos["signal_time_{}".format(c[0])])
-            histos["signal_time_{}".format(c[0])].Add(histos["nPhotons_time_{}".format(c[0])])
-            temp = copy.deepcopy(histos["nPhotons_time_smear_{}".format(c[0])])
-            temp.Scale(10)
-            histos["signal_time_{}".format(c[0])].Add(temp)
+            # #Make realistic waveform plots
+            # histos["signal_time_{}".format(c[0])] = noise1D(histos["signal_time_{}".format(c[0])])
+            # histos["signal_time_{}".format(c[0])].Add(histos["nPhotons_time_{}".format(c[0])])
+            # temp = copy.deepcopy(histos["nPhotons_time_smear_{}".format(c[0])])
+            # temp.Scale(8)
+            # histos["signal_time_{}".format(c[0])].Add(temp)
 
             # Fill the nPhotons per channel summary histogram
             h = histos["dummy_nPhotons_xy_{}".format(c[0])]
+            #print(h.GetEntries())
             for bx in range(1, h.GetXaxis().GetNbins()+1):
                 for by in range(1, h.GetYaxis().GetNbins()+1):
                     nPhotonsPerChannel = h.GetBinContent(bx, by)
+                    #h.SetBinContent(bx, by, 0)
                     #if nPhotonsPerChannel == 0: continue
                     histos["nPhotonsPerChannel_{}".format(c[0])].Fill(nPhotonsPerChannel)
-                    h.SetBinContent(bx, by, 0)
-
-        break
+            histos["dummy_nPhotons_xy_{}".format(c[0])].Reset()
             
     # Draw and save histos
     c1 = ROOT.TCanvas( "c", "c", 800, 700)
@@ -157,8 +168,8 @@ def main():
     for c in nChannels: saveHisto(c1, histos, "nPhotons_xy_{}".format(c[0]), "colz")
     for c in nChannels: saveHisto(c1, histos, "nPhotonsPerChannel_{}".format(c[0]),"hist", True)
     for c in nChannels: saveHisto(c1, histos, "nPhotons_time_{}".format(c[0]),"hist")
-    for c in nChannels: saveHisto(c1, histos, "nPhotons_time_smear_{}".format(c[0]),"hist")
-    for c in nChannels: saveHisto(c1, histos, "signal_time_{}".format(c[0]),"hist")
+    #for c in nChannels: saveHisto(c1, histos, "nPhotons_time_smear_{}".format(c[0]),"hist")
+    #for c in nChannels: saveHisto(c1, histos, "signal_time_{}".format(c[0]),"hist")
 
     # Write everything out
     root_file.Write()
